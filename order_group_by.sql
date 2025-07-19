2b4fd6d6f20e50db17bac26b907fe8ba411a3bf4
Abdullah Khan <abdullahjan2780@gmail.com>
2025-07-19 07:46:54 +0500

    howto_order_group

---
+++ order_group_by.sql
@@ -0,0 +1,14 @@
+-- Group BY
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender;
+
+SELECT occupation, salary
+FROM employee_salary
+GROUP BY occupation, salary;
+
+-- Order By
+SELECT *
+FROM employee_demographics
+ORDER BY gender, age DESC;
